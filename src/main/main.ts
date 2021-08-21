e899f37fd4d0602b0e3746e912de96d2cdd062dc
Amila Welihinda <amilajack@gmail.com>
2021-08-20 17:09:53 -0700

    feat: add sqlite3 to main process

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import path from 'path';
 import { app, BrowserWindow, shell, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
+import sqlite from 'sqlite3';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
@@ -24,6 +25,25 @@ export default class AppUpdater {
     autoUpdater.checkForUpdatesAndNotify();
   }
 }
+
+const sqlite3 = sqlite.verbose();
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run('CREATE TABLE lorem (info TEXT)');
+
+  const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+  for (let i = 0; i < 10; i += 1) {
+    stmt.run(`Ipsum ${i}`);
+  }
+  stmt.finalize();
+
+  db.each('SELECT rowid AS id, info FROM lorem', (_err, row) => {
+    console.log(`${row.id}: ${row.info}`);
+  });
+});
+
+db.close();
 
 let mainWindow: BrowserWindow | null = null;
 
